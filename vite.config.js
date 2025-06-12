77c9157af3fbce1fcc03969408c6024c96723ca1
eacollectibles <ea.collectibles.service@gmail.com>
2025-06-12 13:32:24 -0700

    Update vite.config.js
    changed line in coding

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ✅ FIX: ensures Vite uses relative paths
+  base: './',
   plugins: [react()]
 })
